7d30d875bc7309e5191d15f0c20b678ff424e462
Jakob <mail@bauer-jakob.de>
2024-02-07 21:51:43 +0100

    Initial Commit

---
+++ SafeGuard/SafeGuardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafeGuardApp.swift
+//  SafeGuard
+//
+//  Created by Jakob Bauer on 07.02.24.
+//
+
+import SwiftUI
+
+@main
+struct SafeGuardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
